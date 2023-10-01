c8028875a8694e2759b2574cdcbb38bcdc885fd9
Cristian Stoica <cristianstoica85@gmail.com>
2023-10-01 09:08:01 +0300

    Bumped version to 0.2.0.
    Updated program description.
    Updated CHANGELOG.md file.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,17 @@
 ﻿# Changelog
 
+## 0.2.0 (2023-10-01)
+
+- Prompt user whether to replace existing files when unpacking.
+- Improved Shell Context Menu integration.
+- Display Pre-build Command and Post-build Command information from PLGX files.
+- Added Copy File Info to Clipboard functionality.
+- Added Check for Updates functionality.
+- Added application icon.
+- Updated PlgxUnpackerNet package to the latest version 0.3.0.
+- Other UI enhancements.
+- Other code improvements.
+
 ## 0.1.0 (2023-09-28)
 
 - First version. No release history before this.
--- PlgxUnpacker.WinForms/Properties/AssemblyInfo.cs
+++ PlgxUnpacker.WinForms/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PlgxUnpacker")]
-[assembly: AssemblyDescription("A Windows tool for unpacking KeePass PLGX files.")]
+[assembly: AssemblyDescription("A Windows GUI tool for unpacking KeePass PLGX files.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Cristian Stoica")]
 [assembly: AssemblyProduct("PlgxUnpacker")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
